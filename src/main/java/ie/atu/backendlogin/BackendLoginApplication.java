3fa1b8d5a727b1f9a623beb6eb1fd67ba69eb454
Aidan Malone <g00423306@atu.ie>
2025-01-04 19:43:20 +0000

    Created backend login microservice

---
+++ src/main/java/ie/atu/backendlogin/BackendLoginApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.backendlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLoginApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ie/atu/backendlogin/User.java
@@ -0,0 +1,28 @@
+package ie.atu.backendlogin;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    private String email;
+
+}
